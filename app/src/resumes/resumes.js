ca29d7b3a1f884cb52787b7ebd75260dac42e2b0
hasparus <hasparus@gmail.com>
2018-10-10 16:19:48 +0200

    Move entire code to `app/`; add README.

---
+++ app/src/resumes/resumes.js
@@ -0,0 +1,8 @@
+// If you create a new resume, import it here:
+import './left-right.vue';
+import './oblique.vue';
+import './side-bar.vue';
+import './side-bar-rtl.vue';
+import './left-right-rtl.vue';
+import './creative.vue';
+import './cool.vue';
